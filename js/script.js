fab72f9cdc767c04a7eb508d0c1a602567dfead7
ulyana <uljka@yahoo.com>
2016-05-03 23:32:42 +0000

    vj

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
-
-
 $(function() {
     $('#btn-send').click(function() {
         $.ajax({

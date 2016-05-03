1f433a90addfd615c436a6e81b69087ea913fa90
ulyana <uljka@yahoo.com>
2016-05-03 21:10:03 +0000

    dhf

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-$('#header').animateCss('pulse');
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
 
 
 $(function() {
